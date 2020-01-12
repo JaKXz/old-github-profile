850684eed279dde392b0f11ef5fd7d9f4c73b3e1
Jason Kurian <jason@paddlehr.com>
2020-01-12 04:07:06 -0500

    remove unneeded code and add remark plugins

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,7 +6,15 @@ module.exports = {
   },
   plugins: [
     `gatsby-plugin-theme-ui`,
-    `gatsby-plugin-mdx`,
+    {
+      resolve: `gatsby-plugin-mdx`,
+      options: {
+        remarkPlugins: [
+          require("remark-breaks"),
+          require("remark-external-links"),
+        ],
+      },
+    },
     `gatsby-plugin-react-helmet`,
     {
       resolve: `gatsby-source-filesystem`,
@@ -20,11 +28,9 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
+        name: `jason-kurian-portfolio`,
+        short_name: `jk-portfolio`,
         start_url: `/`,
-        background_color: `#663399`,
-        theme_color: `#663399`,
         display: `minimal-ui`,
         icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
       },
