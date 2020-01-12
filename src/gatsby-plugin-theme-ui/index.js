f038934da6848a921336ad5ce2075d78b9b3f0f9
Jason Kurian <jason@paddlehr.com>
2020-01-11 22:27:41 -0500

    add Libre Baskerville and Open Sans fonts from Google
    Use Cochin on Macs for the feel I actually want

--- src/gatsby-plugin-theme-ui/index.js
+++ src/gatsby-plugin-theme-ui/index.js
@@ -2,4 +2,9 @@ import theme from "@theme-ui/preset-base";
 
 export default {
   ...theme,
+  fonts: {
+    ...theme.fonts,
+    heading: `'Cochin', 'Libre Baskerville', serif`,
+    body: `'Open Sans', ${theme.fonts.body}`,
+  },
 };
