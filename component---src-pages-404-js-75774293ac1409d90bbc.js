03a7eea8b15e14c168b0a69e99501971f754a6ae
Jason Kurian <jasonk92@gmail.com>
2020-01-13 03:25:39 -0500

    364606a

---
+++ component---src-pages-404-js-75774293ac1409d90bbc.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[4],{w2l6:function(e,t,n){"use strict";n.r(t),n.d(t,"default",(function(){return o}));var a=n("q1tI"),u=n.n(a),l=n("Bl7J"),r=n("vrFN");function o(){return u.a.createElement(l.a,null,(function(){return u.a.createElement(u.a.Fragment,null,u.a.createElement(r.a,{title:"404: Not found"}),u.a.createElement("h1",null,"NOT FOUND"),u.a.createElement("p",null,"You just hit a route that doesn't exist... the sadness."))}))}}}]);
+//# sourceMappingURL=component---src-pages-404-js-75774293ac1409d90bbc.js.map
